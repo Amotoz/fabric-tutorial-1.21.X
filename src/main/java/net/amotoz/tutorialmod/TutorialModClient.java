03d677a9a5537d960fbe8fe420e81079ecbbb935
Amotoz <amotozg@gmail.com>
2025-07-28 20:02:29 -0230

    first commit :)

---
+++ src/main/java/net/amotoz/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.amotoz.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
